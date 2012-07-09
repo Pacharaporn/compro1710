c5f9968126c54a78394159694a0332c74a9100a2
Pacharaporn <mayy_coolzy@hotmail.com>
2012-07-09 15:30:45 +0700

    Programming is fun!!

--- src/th/ac/nu/students/u54341710/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54341710/compro/ch01/HelloWorld.java
@@ -2,6 +2,6 @@ package th.ac.nu.students.u54341710.compro.ch01;
 public class HelloWorld {
     public static void main(String[]args){
         System.out.println("Hello,World");
-       
+        System.out.println("Programming is fun!!");
         }
  }
